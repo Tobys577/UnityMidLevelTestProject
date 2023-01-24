1c4993e2afbf711cd51f366d465c7abd19cc2602
Tobyshull <112322872+Tobys577@users.noreply.github.com>
2023-01-24 20:11:58 +0000

    Implemented menu and firebase connections

--- UnityMidLevel/Assets/Scripts/Menu/MenuController.cs
+++ UnityMidLevel/Assets/Scripts/Menu/MenuController.cs
@@ -1,6 +1,11 @@
+using Firebase.Database;
+using Firebase.Extensions;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Newtonsoft.Json.Linq;
 
 namespace UnityMidLevel.Menu
 {
@@ -17,9 +22,79 @@ namespace UnityMidLevel.Menu
         [SerializeField]
         private GameObject[] buttonView;
 
+        [Tooltip("The default value for gravity, if no playerpref or firestore is found")]
+        [SerializeField]
+        private float defaultGravityValue;
+
+        [SerializeField]
+        private TMP_InputField gravityInputField;
+
         void Start()
         {
+            // Load the gravity value from firebase
+            FirebaseDatabase.DefaultInstance.GetReference("Gravity")
+                .GetValueAsync().ContinueWithOnMainThread(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Debug.LogError(task.Exception);
+                    } else if (task.IsCompleted)
+                    {
+                        DataSnapshot data = task.Result;
+                        if(data.Value == null)
+                        {
+                            Debug.Log("No data!");
+                            float playerPrefGravity = PlayerPrefs.GetFloat("Gravity", -999);
+                            if (playerPrefGravity == -999)
+                            {
+                                gravityInputField.text = defaultGravityValue.ToString();
+                            } else
+                            {
+                                gravityInputField.text = playerPrefGravity.ToString();
+                                Debug.Log("gravity input field loaded from playerpref");
+                            }
+                        } else
+                        {
+                            gravityInputField.text = data.Value.ToString();
+                            Debug.Log("gravity input field loaded from firebase");
+                        }
+                    } else
+                    {
+                        Debug.LogWarning("Grativty fetch task is not complete");
+                    }
+                });
+
             LoadButtonView(defaultButtonView);
+        }
+
+        /// <summary>
+        /// Function ran when the gravity input field is modified
+        /// Used to keep the value above the minimum of 1
+        /// </summary>
+        public void GravityInputFieldValueChanged()
+        {
+            float gravityInputFieldTryParse = -1.0f;
+            if (float.TryParse(gravityInputField.text, out gravityInputFieldTryParse))
+            {
+                if(gravityInputFieldTryParse < 1)
+                    gravityInputField.text = "1.0";
+            }
+        }
+
+        /// <summary>
+        /// Save the values in the gravity input field
+        /// Called by the "SaveButton" from the menu
+        /// </summary>
+        public void SaveGravityInputField()
+        {
+            float gravityInputFieldTryParse = -1.0f;
+            if (!float.TryParse(gravityInputField.text, out gravityInputFieldTryParse))
+            {
+                gravityInputFieldTryParse = defaultGravityValue;
+            }
+
+            PlayerPrefs.SetFloat("Gravity", gravityInputFieldTryParse);
+            FirebaseDatabase.DefaultInstance.GetReference("Gravity").SetValueAsync(gravityInputFieldTryParse);
         }
 
         /// <summary>
